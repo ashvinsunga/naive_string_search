079ef0a8e5132408fcbdbad7833661a071b3af17
ashvinsunga <ashvinsunga@yahoo.com.ph>
2021-09-08 19:33:22 +0800

    done

--- script.js
+++ script.js
@@ -10,3 +10,20 @@
 // If you complete the inner loop and find a match, increment the count of matches
 // Return the count
 
+// Solution 1
+function naiveSearch(long, short){
+    var count = 0;
+    for(var i = 0; i < long.length; i++){
+        for(var j = 0; j < short.length; j++){
+            if(short[j] !== long[i+j]){
+                break;
+            }
+            if (j === short.length - 1) {
+                count++;
+            }
+        }
+        
+    }
+return count;
+}
+console.log(naiveSearch("lorie loled", "lo"));
