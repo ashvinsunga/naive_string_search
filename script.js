e6fe97a70f840b6a770021434209203a50a38a70
ashvinsunga <ashvinsunga@yahoo.com.ph>
2021-09-08 16:42:12 +0800

    comment added

---
+++ script.js
@@ -0,0 +1,12 @@
+// THE GOAL OF THIS FUNCTION
+// Count the number of times a smaller string appears in a longer string
+// A straightforward approach involves checking pairs of characters individually
+
+// Pseudocode
+// Loop over the longer string
+// Loop over the shorter string
+// If the characters don't match, break out the inner loop
+// if the characters do match, keep going
+// If you complete the inner loop and find a match, increment the count of matches
+// Return the count
+
